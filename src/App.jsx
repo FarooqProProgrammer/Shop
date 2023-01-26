e5bd3c37295b3c292b9345c5526e125fee26e52b
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-01-25 23:49:21 -0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Products from './pages/Product'
+import SingleProduct from './pages/SingleProduct'
 
 
 
